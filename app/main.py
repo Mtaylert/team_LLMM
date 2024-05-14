6f118cedd74938144aa7d8c5d846511f29f2e78f
Mtaylert <Mtaylert@drift.com>
2024-05-14 16:44:45 -0600

    chunking

--- app/main.py
+++ app/main.py
@@ -46,11 +46,33 @@ def dynamic_chunking():
 
 
 def semantic_chunking():
-    return ""
+    chunk = """In July, Alexander-Walker agreed to a two-year, $9 million contract with Minnesota. 
+    For the first time in his career, the role he wanted and the one his team needed were in agreement.
+    """
+    return chunk
 
 
 def basic_chunking():
-    return ""
+    basic_chunk = """
+    AFTER BEING DRAFTED by New Orleans and traded three times, Alexander-Walker finally had a choice of where he wanted to play when he became a free agent last summer.
+
+    Remaining in Minnesota would give him the opportunity to do something he'd never done before either: start the season with the same coach he finished the previous year with.
+    
+    Finch said the team pitched him on both in an effort to bring him back.
+    
+    "It was so important to re-sign him," Finch said in December. "We saw what he could do to help this team last year down the stretch. He feels really comfortable here. I think we have a greater appreciation for who he is as a player. Now, the defense is way better than we had anticipated, and he understands that that's his ticket to making an impact on the floor."
+    
+    In July, Alexander-Walker agreed to a two-year, $9 million contract with Minnesota. For the first time in his career, the role he wanted and the one his team needed were in agreement.
+    
+    "It was a two-way buy-in," Boylan says. "We bought into him, but he bought into us at the same time."
+    
+    His teammates have bought in too.
+    
+    
+    "For me, I respect work more than anything," Gobert tells ESPN. "And first of all, I love the way he works every day. And that translates to the court ... every minute that he is on the court. He takes every play serious. He takes every possession serious. ... Everything that he does has a purpose."
+    
+    """
+    return basic_chunk
 
 
 @app.post("/question-answering")
